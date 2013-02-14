4c18050464d7abc333f1d1cfb57f880f84a316a9
ram9 <curtis@ram9.cc>
2013-02-14 11:08:33 -0800

    Added rails project that i used to verify fonts functioning on IOS, Safari, Chrome, IE8, IE9

---
+++ fonts-rails-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fonts::Application.config.secret_token = '8e49140c5746004cce1dcad974f97770f4a4b1af04be69889e38f4d15c456d76ae30899398652c67df3a8f5739eee4df837566866e2ed0c49c37783c95af6224'
